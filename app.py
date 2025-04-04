15ffe73d48af627f92c773cb1659d7b377cc8728
Aryan <aryanmanchanda@hotmail.com>
2025-04-04 10:23:07 +0530

    login and register

--- app.py
+++ app.py
@@ -12,5 +12,13 @@ def home():
 def dashboard():
     return render_template("dashboard.html")
 
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
